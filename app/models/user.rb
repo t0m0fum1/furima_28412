3dcbf7b9e0490fe39b4d26b690ae747f71de17e0
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-13 15:30:01 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :name, presence: true
+         validates :firstname, presence: true
+         validates :lastname, presence: true
+         validates :firstname_furigana, presence: true
+         validates :lastname_furigana, presence: true
+         validates :birthday, presence: true
 end
