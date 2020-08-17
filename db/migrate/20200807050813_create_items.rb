f9ba773349f1f518c9d049b4ed418308fc12b179
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-18 00:18:35 +0900

    出品ページへのリンク、active_hashを導入、カテゴリのモデル作成

--- db/migrate/20200807050813_create_items.rb
+++ db/migrate/20200807050813_create_items.rb
@@ -1,7 +1,13 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,               null: false
+      t.integer :burden,            null: false
+      t.integer :fee,            null: false
+      t.string :name,            null: false
+      t.string :name,            null: false
+      t.string :name,            null: false
+      t.string :name,            null: false
       t.timestamps
     end
   end
