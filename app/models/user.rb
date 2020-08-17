7b970881ad64a94833896d42bffec048fffae578
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-17 16:38:11 +0900

    修正1回目（テストコード整理、パスワードのバリデーション設定、テストコードの見直し）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates_format_of :password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 40 }
-  validates :firstname, presence: true, format: { with: /\A[一-龥]+\z/ }
-  validates :lastname, presence: true, format: { with: /\A[一-龥]+\z/ }
-  validates :firstname_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :lastname_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+
+  with_options presence: true do |existence|
+    existence.validates :firstname, format: { with: /\A[一-龥]+\z/ }
+    existence.validates :lastname, format: { with: /\A[一-龥]+\z/ }
+  end
+  
+  with_options presence: true do |existence|
+    existence.validates :firstname_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    existence.validates :lastname_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
