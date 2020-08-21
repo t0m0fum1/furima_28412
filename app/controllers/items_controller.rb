7a3bde496692ac4ee0bf9136f121f68b2c1c2554
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-21 15:36:23 +0900

    修正2回目
    before_actionでeditとshowの記述をまとめました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+
+  before_action :set_item, only: [:edit, :show]
+
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -26,16 +29,20 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
-
+  
   def show
-    @item = Item.find(params[:id])
   end
-
+  
   private
-
+  
   def item_params
     params.require(:item).permit(:image, :category_id, :status_id, :burden_id, :shipping_origin_id, :day_id, :fee, :name, :explain).merge(user_id: current_user.id)
   end
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
