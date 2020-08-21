9d1b5a2097e39d49fea62c6afb9640acf4729595
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-21 15:40:34 +0900

    修正2回目追記
    destoroyもインスタンス変数を利用し1つにまとめました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :destroy]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
@@ -20,9 +20,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    if item.destroy
+    @item.destroy
+    if @item.destroy
       redirect_to root_path
     else
       render :show
