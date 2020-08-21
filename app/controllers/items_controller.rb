8643f2b8c14113d50cff05bdf486c763bd50bdd0
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-21 13:12:52 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   def edit
     @item = Item.find(params[:id])
   end
