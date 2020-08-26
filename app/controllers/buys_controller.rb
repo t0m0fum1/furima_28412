6ba5bfe404798446f4ca53edc7d761acc250a0f1
岩瀬智史 <iwasesatoshi@iwasesatoshinoMacBook-Air.local>
2020-08-26 14:50:29 +0900

    クレジット決済実装（仮）

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,11 +1,37 @@
 class BuysController < ApplicationController
   
   def new
+    @buy = Buy.new
     @item = Item.find(params[:item_id])
   end
 
   def create
-    
+    @buy = Buy.new(buy_params)
+    if @buy.valid?
+      pay_item
+      @buy.save
+      return redirect_to root_path
+    else
+      render 'new'
+    end
   end
   
+
+  private
+
+  def buy_params
+    params.permit(:token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: buy_params[],
+      card: buy_params[:token],
+      currency:'jpy'
+    )
+  end
+
+
+
 end
